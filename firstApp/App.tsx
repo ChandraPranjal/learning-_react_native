f6acd6ee5b8646a2e9874c8ca7b682365ceaa275
ChandraPranjal <iampranjalchandra@gmail.com>
2024-03-14 18:05:11 +0530

    first

---
+++ firstApp/App.tsx
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default App;
